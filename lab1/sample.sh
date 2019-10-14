a4700457967f5c46a6086f5ca1e551afa02f5b76
foodsnow <dut.kht@gmail.com>
2019-10-14 21:26:26 +0600

    first

---
+++ lab1/sample.sh
@@ -0,0 +1,15 @@
+#!bin/bash
+
+var1=$(grep -i '11/Oct/2006' log.txt |  awk -F '" "' '{print $2}' | cut -d '(' -f 1  | sort | uniq -c | sort -nr -k 1 | sed 's/"//g' | awk '{sum+=$1}END{print sum}')
+
+red=$(grep -i '11/Oct/2006' log.txt |  awk -F '" "' '{print $2}' | cut -d '(' -f 1  | sort | uniq -c | sort -nr -k 1 | sed 's/"//g' | awk -v x=$var1 '{printf "%s - %.0f - %0.f%%\n", $2, $1, $1/x*100}' | head -n 10)
+
+echo "$red"
+
+
+
+
+
+
+
+
